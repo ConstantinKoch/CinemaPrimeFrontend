5268a1f845373d96ce0822b8257085dfa25f284e
MrChrisse <chrilei2000@gmail.com>
2021-01-16 11:02:54 +0100

    unfinished DetailMoviePage

--- frontend/src/Body/DetailMoviePage/DetailMoviePage.tsx
+++ frontend/src/Body/DetailMoviePage/DetailMoviePage.tsx
@@ -4,35 +4,40 @@ import axios from 'axios';
 import Movie from '../../Models/Movie';
 
 interface IProps {
-	movie: Movie;
 }
 
 interface IState {
 	runningMovies?: Movie[];
+	url?: String;
+	lastSegment?: number;
+	movie?: Movie;
 }
 
 export default class DetailMoviePage extends Component<IProps, IState> {
-	state: IState = {};
-
-	url = window.location.href;
-	lastSegment = this.url.split('/').pop();
-
-	Movie_temp: Movie | undefined;
-
+	state:IState = ({url : window.location.href})
 	componentDidMount() {
 		axios.get<Movie[]>('http://localhost:8080/tmdb/running/').then((res) => res.data).then((result) => {
 			this.setState({
 				runningMovies: result
 			});
 		});
+		this.setState({lastSegment : Number(this.state.url?.split('/').pop())})
+		
+		this.state.runningMovies?.filter(movie => movie.id === this.state.lastSegment)
+		this.setState({movie : this.state.runningMovies?.pop()})
 	}
-
-	//Movie_temp = this.runningMovies.map((i))
+	
 
 	render() {
+		console.log(this.state.movie?.title)
+		console.log(this.state.runningMovies?.length)
+
 		return (
 			<div>
-				<h1>{this.lastSegment}</h1>
+				<h1>{this.state.lastSegment}</h1>
+				<p>{this.state.movie?.title}</p>
+				<p>{this.state.runningMovies?.pop()?.title}</p>
+				
 				{/*<img
 						className="movieImg"
 						src={'https://image.tmdb.org/t/p/w500' + this.props.movie.poster_path}
--- frontend/src/Body/DetailMoviePage/dist/DetailMoviePage.js
+++ frontend/src/Body/DetailMoviePage/dist/DetailMoviePage.js
@@ -20,23 +20,29 @@ var DetailMoviePage = /** @class */ (function (_super) {
     __extends(DetailMoviePage, _super);
     function DetailMoviePage() {
         var _this = _super !== null && _super.apply(this, arguments) || this;
-        _this.state = {};
-        _this.url = window.location.href;
-        _this.lastSegment = _this.url.split('/').pop();
+        _this.state = ({ url: window.location.href });
         return _this;
     }
     DetailMoviePage.prototype.componentDidMount = function () {
         var _this = this;
+        var _a, _b, _c;
         axios_1["default"].get('http://localhost:8080/tmdb/running/').then(function (res) { return res.data; }).then(function (result) {
             _this.setState({
                 runningMovies: result
             });
         });
+        this.setState({ lastSegment: Number((_a = this.state.url) === null || _a === void 0 ? void 0 : _a.split('/').pop()) });
+        (_b = this.state.runningMovies) === null || _b === void 0 ? void 0 : _b.filter(function (movie) { return movie.id === _this.state.lastSegment; });
+        this.setState({ movie: (_c = this.state.runningMovies) === null || _c === void 0 ? void 0 : _c.pop() });
     };
-    //Movie_temp = this.runningMovies.map((i))
     DetailMoviePage.prototype.render = function () {
+        var _a, _b, _c, _d, _e;
+        console.log((_a = this.state.movie) === null || _a === void 0 ? void 0 : _a.title);
+        console.log((_b = this.state.runningMovies) === null || _b === void 0 ? void 0 : _b.length);
         return (react_1["default"].createElement("div", null,
-            react_1["default"].createElement("h1", null, this.lastSegment)));
+            react_1["default"].createElement("h1", null, this.state.lastSegment),
+            react_1["default"].createElement("p", null, (_c = this.state.movie) === null || _c === void 0 ? void 0 : _c.title),
+            react_1["default"].createElement("p", null, (_e = (_d = this.state.runningMovies) === null || _d === void 0 ? void 0 : _d.pop()) === null || _e === void 0 ? void 0 : _e.title)));
     };
     return DetailMoviePage;
 }(react_1.Component));
