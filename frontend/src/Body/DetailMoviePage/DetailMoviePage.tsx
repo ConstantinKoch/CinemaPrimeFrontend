9667e38acfe893b0e18d5258802ca57480178f8d
MrChrisse <chrilei2000@gmail.com>
2021-01-19 20:24:42 +0100

    changed DetailMovieView (YT)

--- frontend/src/Body/DetailMoviePage/DetailMoviePage.tsx
+++ frontend/src/Body/DetailMoviePage/DetailMoviePage.tsx
@@ -10,7 +10,7 @@ import { Link } from 'react-router-dom';
 import YTSearch from 'youtube-api-search';
 import ReactPlayer from "react-player"
 
-let API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+let API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 interface IProps {
 }
@@ -21,7 +21,7 @@ interface IState {
 	lastSegment?: number;
 	movie?: Movie;
 	book_path?: String;
-	selected_date?: String;
+	selected_date: String;
 	videos_arr?: any[];
 	displayVideo?: any;
 }
@@ -34,7 +34,13 @@ const options = [
   ];
 
 export default class DetailMoviePage extends Component<IProps, IState> {
-	state:IState = ({url : window.location.href})
+	state:IState = ({url : window.location.href, selected_date : '11_01_20'})
+
+	constructor(IProps: IProps | Readonly<IProps>){
+		super(IProps);
+		this.handleChange = this.handleChange.bind(this)
+		this.videoSearch = this.videoSearch.bind(this)
+	}
 	componentDidMount() {
 		axios.get<Movie[]>('http://localhost:8080/tmdb/running/').then((res) => res.data)
 		.then((result) => {
@@ -43,17 +49,18 @@ export default class DetailMoviePage extends Component<IProps, IState> {
 			});
 		}).then((): void => {
 		this.setState({lastSegment : Number(this.state.url?.split('/').pop())})
-		this.setState({movie : this.state.runningMovies?.filter(movie => movie.id === this.state.lastSegment).pop()})} )
+		this.setState({movie : this.state.runningMovies?.filter(movie => movie.id === this.state.lastSegment).pop()}) 
+		this.videoSearch()})
 	}
 
 	handleChange(e: any) {
-		console.log("Fruit Selected!!");
+		console.log("Datum geändert");
 		this.setState({ selected_date : e.target.value });
 	}
 
 	videoSearch = () => {
 		const term_str = this.state.movie?.title + " Trailer";
-		YTSearch({key: API_KEY, term: term_str}, (videos: any[]) => {
+		YTSearch({key: API_KEY, term: term_str, maxResults: 1}, (videos: any[]) => {
 			this.setState({
 				videos_arr : videos,
 				displayVideo : videos[0]
@@ -62,7 +69,7 @@ export default class DetailMoviePage extends Component<IProps, IState> {
 	}
 	
 	render(): JSX.Element {
-		this.videoSearch()
+		console.log(this.state.videos_arr)
 		return (
 			<Grid
 			container
@@ -78,7 +85,7 @@ export default class DetailMoviePage extends Component<IProps, IState> {
 						src={'https://image.tmdb.org/t/p/w500' + this.state.movie?.poster_path}
 						alt={this.state.movie?.title + ' Movie Poster'}/>
 
-				<ReactPlayer>this.state.displayVideo</ReactPlayer>
+				<ReactPlayer url={`https://www.youtube.com/watch?v=${this.state.displayVideo?.id.videoid}`} />
 				</div>
 			</Grid>
 
@@ -102,7 +109,7 @@ export default class DetailMoviePage extends Component<IProps, IState> {
 						onChange={this.handleChange}
         			/>
 					<div className="movieDetails_btns_div">
-						<Button className="movieDetails_btn" variant="contained">
+						<Button className="movieDetails_btn_trailer" variant="contained">
 							Trailer
 						</Button>
 						<Link className="movieLink_booking" to={{pathname: (this.state.movie?.id!==undefined ? 
